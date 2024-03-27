edf09726083d4841777c39ca4cd0fbcdd3bbcf9b
mohammed salah <mohammed.salah.hassan@hotmail.com>
2024-03-27 22:27:05 +0000

    tasks

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""THIS IS A DESCRIPTION"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    ID = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(ID)
+    url_todos = url + '/todos'
+
+    user_info = requests.get(url).json()
+    todos_info = requests.get(url_todos).json()
+
+    Name = user_info.get("username")
+    
+    User_Data = {
+        ID:[
+        {
+            "task": t.get("title")
+            "completed": t.get("completed")
+            "username": Name
+        }
+        for t in todos_info
+        ]
+    }
+
+    FileName = "{}.json".format(user_id)
+    with open(FileName, "w") as jsonfile:
+        json.dump(User_Data, jsonfile, indent=4)
